c532b61fbbc09cd68dba1fa2394a44f467f63da1
Eyo Elijah <eyoelijah@gmail.com>
2021-08-25 15:38:13 +0100

    added the delete file

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,38 @@
+const { MongoClient, ObjectID } = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        return console.log('Unable to connect MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+
+    // deleteMany
+    // db.collection('Todo').deleteMany({
+    //     text: 'Eat lunch'
+    // }).then((result) => {
+    //     console.log(result);
+    // });
+
+    // deleteOne
+    // db.collection('Todo').deleteOne({
+    //     text: 'Eat lunch'
+    // }).then((result) => {
+    //     console.log(result);
+    // });
+
+    // findOneAndDelete
+    db.collection('Todo').findOneAndDelete({
+        completed: false
+    }).then((result) => {
+        console.log(result);
+    });
+    // user collection
+    db.collection('User').findOneAndDelete({
+        name: 'Eyo Elijah'
+    }).then((result) => {
+        console.log(result);
+    });
+
+    // db.close();
+
+});
