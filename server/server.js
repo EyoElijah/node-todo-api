f2f4a6d52fcac65e2f19eb3d6cc6f07d7d1e9998
Eyo Elijah <eyoelijah@gmail.com>
2021-08-26 16:44:05 +0100

    Add Post/todos route and refactor mongoose

---
+++ server/server.js
@@ -0,0 +1,25 @@
+const express = require('express'),
+    bodyParser = require('body-parser');
+
+let { mongoose } = require('./db/mongoose');
+let { Todo } = require('./models/todo');
+let { User } = require('./models/user');
+
+let app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// app.use(bodyParser.json());
+app.post('/todos', (req, res) => {
+    let todo = new Todo({
+        text: req.body.text
+    });
+    todo.save().then((doc) => {
+        res.send(doc)
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Started on port 3000');
+})
