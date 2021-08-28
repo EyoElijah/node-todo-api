dd4b73bc4810f4d9ff81e68836f208b5e68564b1
Eyo Elijah <eyoelijah@gmail.com>
2021-08-28 16:49:50 +0100

    Add Get /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require('express'),
     bodyParser = require('body-parser');
 
+const { ObjectID } = require('mongodb');
+
 let { mongoose } = require('./db/mongoose');
 let { Todo } = require('./models/todo');
 let { User } = require('./models/user');
@@ -27,6 +29,24 @@ app.get('/todos', (req, res) => {
     }, (e) => {
         res.status(400).send(e);
     })
+});
+
+app.get('/todos/:id', (req, res) => {
+    let id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(400).send();
+    }
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+        res.send({
+            todo
+        })
+    }).catch((error) => {
+        res.status(404).send()
+    });
 });
 
 app.listen(3000, () => {
